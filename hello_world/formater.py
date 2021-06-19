33c1dbf11395223b1d79c08b2c9581bf46c99a4c
Jula90210 <Jula90210@users.noreply.github.com>
2021-06-19 09:37:11 +0000

    Add change in formater.py

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -3,7 +3,6 @@ PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
-XML = "xml"
 
 SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON, XML]
 
@@ -18,8 +17,6 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
-    elif format == XML:
-        result = format_to_xml(msg, imie)
     return result
 
 
@@ -27,9 +24,6 @@ def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "msg":"' +
             msg + '"}')
 
-def format_to_xml(msg, imie):
-    return ('{ "imie":"' + imie + '", "msg":"' +
-            msg + '"}')
 
 def plain_text(msg, imie):
     return imie + ' ' + msg
